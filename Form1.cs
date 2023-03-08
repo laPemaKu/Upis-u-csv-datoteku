6d9b6e7129c1cc993c93da101d19bda41c46326c
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-03-08 09:14:31 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upisivanje_podataka_u_csv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnos_Click(object sender, EventArgs e)
+        {
+            List<Osoba> listaosoba = new List<Osoba>();
+            try
+            {
+                Osoba osoba = new Osoba(txtIme.Text, txtPrezime.Text, txtEmail.Text,Convert.ToInt16(txtGodRod.Text));
+                txtIme.Clear();
+                txtPrezime.Clear();
+                txtEmail.Clear();
+                txtGodRod.Clear();
+
+                DialogResult upit = MessageBox.Show("Želite li unesti još podataka?", "Upit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                switch (upit)
+                {
+                    case DialogResult.Yes:
+                        {
+                            listaosoba.Add(osoba);
+                            break;
+                        }
+                    case DialogResult.No:
+                        {
+                            listaosoba.Add(osoba);
+                            //upis podataka u datoteku
+                            using (var writer = new StreamWriter("osobe.csv"))
+                            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                            {
+                                csv.WriteRecords(listaosoba);
+                            }
+
+                            break;
+                        }
+                }
+
+            }catch(Exception greska)
+            {
+                MessageBox.Show(greska.Message, "Pogrešab unos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Osoba.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upisivanje_podataka_u_csv
+{
+    internal class Osoba
+    {
+        string ime, prezime, email;
+        int godRod;
+
+        public Osoba(string ime, string prezime, string email, int godRod)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.email = email;
+            this.godRod = godRod;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Ime: " + this.ime + " Prezime: " + this.prezime
+                + " Godina rođenja: " + this.godRod + " Email: " + this.email;
+            return ispis;
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public string Email { get => email; set => email = value; }
+        public int GodRod { get => godRod; set => godRod = value; }
+    }
+}
